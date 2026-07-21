287d1e98d74a3c3017dd434964b73789ec271130
hanarental07-max <hanarental07@gmail.com>
2026-07-21 14:01:59 +0900

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,22 @@
 
+let products=[];
+
+
+
+async function loadProducts(){
+
+const response =
+await fetch(
+"data/products.json"
+);
+
+
+products =
+await response.json();
+
+
+}
+
+
+
+loadProducts();
