b3ef2cb9a21e492757ac70a47c97d823fe561fc3
hanarental07-max <hanarental07@gmail.com>
2026-07-21 13:53:15 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,101 @@
 
+const startBtn =
+document.getElementById("startBtn");
+
+
+const hero =
+document.getElementById("hero");
+
+
+const questionBox =
+document.getElementById("questionBox");
+
+
+
+startBtn.onclick=function(){
+
+
+hero.style.display="none";
+
+
+questionBox.style.display="block";
+
+
+loadQuestion();
+
+
+};
+
+
+
+const questions=[
+
+{
+
+title:"어떤 업종인가요?",
+
+options:[
+
+"🏢 사무실",
+
+"🏫 학원",
+
+"🏥 병원",
+
+"🏗 건설",
+
+"🏪 매장"
+
+]
+
+}
+
+];
+
+
+
+let current=0;
+
+
+
+function loadQuestion(){
+
+
+const q=questions[current];
+
+
+document.getElementById(
+"questionTitle"
+).innerHTML=q.title;
+
+
+
+let html="";
+
+
+q.options.forEach(item=>{
+
+
+html+=`
+
+<div class="option">
+
+${item}
+
+</div>
+
+`;
+
+
+});
+
+
+document.getElementById(
+"options"
+).innerHTML=html;
+
+
+
+}
+
+
